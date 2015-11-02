6bc0fbadc5aa1d71e4a17c3d25fd7041e17da80a
PedroMello <pedro@localhost.localdomain>
2015-11-01 23:55:35 -0200

    Commit inicial.
    Já realizei algumas etapas do tutorial. Parei na metade da parte em que é ensinado a lidar com diferentes idiomas.

---
+++ app/src/main/java/br/com/apps/pedro/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,31 @@
+package br.com.apps.pedro.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_display_message);
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+}
